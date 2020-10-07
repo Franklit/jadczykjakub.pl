fbe2bcc329ded6fb887b79677d047079755adb01
Jakub <jakub.jadczyk@wp.pl>
2020-10-07 21:06:24 +0200

    blue background, hallo, flags, img man

---
+++ js/main.js
@@ -0,0 +1,60 @@
+//animation first page
+// const tl = gsap.timeline({defaults: {ease: 'power1.out'} });
+
+// tl.to('.text', {y:'0%', duration: 1, stagger: 0.25});
+// tl.to('.slider', {y: '-100%', duration: 1.5, delay: 0.5});
+// tl.to('.intro', {y: '-100%', duration: 1}, '-=1');
+// tl.fromTo('nav', {opacity: 0}, {opacity: 1, duration: 1});
+// tl.fromTo('.big-text', {opacity: 0}, {opacity: 1, duration: 1}, '-=1');
+
+// global variables
+const h1 = document.querySelector('h1');
+const p = document.querySelector('p')
+const lgRadio = document.querySelectorAll('input')
+const label = document.querySelectorAll('label')
+
+
+const divLanguage = document.querySelector('.language')
+divLanguage.addEventListener('click', ()=>{
+    lgRadio.forEach(element => { if(element.checked){
+        label[0].classList.remove('active')
+        label.forEach(el => {
+            el.classList.remove('active')
+        })
+
+        console.log(element.id);
+        let id = element.id;
+        console.log(id)
+
+        h1.textContent = language[`${id}`].welcome;
+        p.textContent = language[`${id}`].p;
+
+        label.forEach(element => {
+            if(element.htmlFor == id){
+                element.classList.add('active')
+            }
+            
+        });
+        // label[1].classList.add('label')
+
+        element.checked = false;
+    }
+        
+    });
+})
+
+//change language
+const language = {
+    pol: {
+        welcome: "Cześć",
+        p : "Nazywam sie"
+    },
+    en: {
+        welcome: "Hi",
+        p : "My name is"
+    },
+    de: {
+        welcome: "Hallo",
+        p : "Ich heisse"
+    }
+}
