b5610588bfa350cba5f6e92ba09684673b4225a5
Jakub <jakub.jadczyk@wp.pl>
2020-10-08 18:05:48 +0200

    languages and it skills ready to style

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 //animation first page
-const tl = gsap.timeline({defaults: {ease: 'power1.out'} });
+// const tl = gsap.timeline({defaults: {ease: 'power1.out'} });
 
-tl.to('.text', {y:'0%', duration: 1, stagger: 0.25});
-tl.to('.slider', {y: '-100%', duration: 1.5, delay: 0.5});
-tl.to('.intro', {y: '-100%', duration: 1}, '-=1');
-tl.fromTo('nav', {opacity: 0}, {opacity: 1, duration: 1});
-tl.fromTo('.firstText', {opacity: 0}, {opacity: 1, duration: 1}, '-=1');
+// tl.to('.text', {y:'0%', duration: 1, stagger: 0.25});
+// tl.to('.slider', {y: '-100%', duration: 1.5, delay: 0.5});
+// tl.to('.intro', {y: '-100%', duration: 1}, '-=1');
+// tl.fromTo('nav', {opacity: 0}, {opacity: 1, duration: 1});
+// tl.fromTo('.firstText', {opacity: 0}, {opacity: 1, duration: 1}, '-=1');
 
 // global variables
 const h1 = document.querySelector('h1');
