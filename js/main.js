93466bffc93079458e962bdbc42b823a3c40dee5
Jakub <jakub.jadczyk@wp.pl>
2020-10-12 14:33:57 +0200

    half language ready

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 //animation first page
-const tl = gsap.timeline({defaults: {ease: 'power1.out'} });
+// const tl = gsap.timeline({defaults: {ease: 'power1.out'} });
 
-tl.to('.text', {y:'0%', duration: 1, stagger: 0.25});
-tl.to('.slider', {y: '-100%', duration: 1.5, delay: 0.5});
-tl.to('.intro', {y: '-100%', duration: 1}, '-=1');
-tl.fromTo('nav', {opacity: 0}, {opacity: 1, duration: 1});
-tl.fromTo('.firstText', {opacity: 0}, {opacity: 1, duration: 1}, '-=1');
+// tl.to('.text', {y:'0%', duration: 1, stagger: 0.25});
+// tl.to('.slider', {y: '-100%', duration: 1.5, delay: 0.5});
+// tl.to('.intro', {y: '-100%', duration: 1}, '-=1');
+// tl.fromTo('nav', {opacity: 0}, {opacity: 1, duration: 1});
+// tl.fromTo('.firstText', {opacity: 0}, {opacity: 1, duration: 1}, '-=1');
 
 //smooth scroll
 const more = document.querySelector('.more a')
@@ -24,28 +24,57 @@ const smoothScrool = (e) =>{
 
 more.addEventListener('click', smoothScrool)
 
-// global variables
-const h1 = document.querySelector('h1');
-const p = document.querySelector('p')
 const lgRadio = document.querySelectorAll('input')
 const label = document.querySelectorAll('label')
-const button = document.querySelector('.landing button')
+
+// global variables from top
+const h1Top = document.querySelector('h1');
+const pTop = document.querySelector('p')
+const aTop = document.querySelector('a span')
+
+
+// variables from Describe
+const h1Desc = document.querySelector('.describe h1') 
+const h2Desc = document.querySelectorAll('.describe h2')
+const h3Desc = document.querySelectorAll('.describe h3')
+// variables from Portfolio
+
+const h1Port = document.querySelector(".portfolio h1")
+
+
 
 const divLanguage = document.querySelector('.language')
 divLanguage.addEventListener('click', ()=>{
     lgRadio.forEach(element => { if(element.checked){
+        //remove last active
         label[0].classList.remove('active')
         label.forEach(el => {
             el.classList.remove('active')
         })
 
-        // console.log(element.id);
-        let id = element.id;
-        // console.log(id)
 
-        h1.textContent = language[`${id}`].welcome;
-        p.textContent = language[`${id}`].p;
-        button.textContent = language[`${id}`].button;
+        let id = element.id;    
+
+        //Top page
+        h1Top.textContent = languages.Top[`${id}`].h1Top;
+        pTop.textContent = languages.Top[`${id}`].pTop;
+        aTop.textContent = languages.Top[`${id}`].aTop
+        
+
+        //describe page
+        h1Desc.textContent = languages.Desc[`${id}`].h1Desc;
+
+        h2Desc.forEach((element, index) => { 
+            element.textContent = languages.Desc[`${id}`].h2Desc[index];   
+        });
+
+
+        h3Desc.forEach((element, index) => {
+            element.textContent = languages.Desc[`${id}`].h3Desc[index];
+        });
+
+        // portfolio page
+        h1Port.textContent = languages.Port[`${id}`].h1Port
 
         label.forEach(element => {
             if(element.htmlFor == id){
@@ -53,7 +82,6 @@ divLanguage.addEventListener('click', ()=>{
             }
             
         });
-        // label[1].classList.add('label')
 
         element.checked = false;
     }
@@ -61,28 +89,69 @@ divLanguage.addEventListener('click', ()=>{
     });
 })
 
-//change language
-const language = {
-    pol: {
-        welcome: "Cześć",
-        p : "Nazywam sie",
-        button: "Wiecej"
+
+
+const languages = {
+    Top: {
+        pol: {
+            h1Top: "Cześć",
+            pTop : "Nazywam sie",
+            aTop: "Zobacz Wiecej"
+        },
+        en: {
+            h1Top: "Hi",
+            pTop : "My name is",
+            aTop: "See More"
+        },
+        de: {
+            h1Top: "Hallo",
+            pTop : "Ich heisse",
+            aTop: "Sieh Mehr"
+        }
+        },
+    Desc: {
+            pol: {
+                h1Desc: "Co potrafie",
+                h2Desc: ["Tworze strony internetowe", "Języki Obce"],
+                h3Desc: ["Nowoczene", "Responsywne", "Estetyczne", "Niemiecki B1", "Angielski C1"],
+                
+
+            },
+            en: {
+                h1Desc: "What can I do",
+                h2Desc: ["I create websites", "foreign language"],
+                h3Desc: ["modern", "Responsive", "aesthetic", "German B1", "English C1"],
+
+            },
+            de: {
+                h1Desc: "Was kann Ich machen",
+                h2Desc: ["Ich erstelle Websites","fremdsprachen"],
+                h3Desc: ["modern", "Reactionschnell", "ästhetisch", "Deutsch B1", "Englisch C1"],
+            }
+        },
+    Port: {
+        pol: {
+            h1Port: "Moje Projekty",
+
+        },
+        en: {
+            h1Port: "My Projects",
+
+
+        },
+        de: {
+            h1Port: "Meine Projekte",
+
+        }
     },
-    en: {
-        welcome: "Hi",
-        p : "My name is",
-        button: "More"
-    },
-    de: {
-        welcome: "Hallo",
-        p : "Ich heisse",
-        button: "Mehr"
-    }
+
 }
 
 
 
-//cubic
+
+
+//cubic interests
 
 const cube = document.querySelector('.cube')
 const interestText = document.querySelector('.interest')
@@ -121,6 +190,5 @@ function clearOrResume(){
         flag = !flag;
     }
 }
-// clearInterval(intervalCube)
-// cube.addEventListener('click', clearInterval(intervalCube) )
+
 cube.addEventListener('click', clearOrResume)
