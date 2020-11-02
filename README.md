8f5928d4c809db5dea426d930b99a4e745961f28
Jakub <jakub.jadczyk@wp.pl>
2020-11-02 13:33:54 +0100

    upadate img, add readme

---
+++ README.md
@@ -0,0 +1,14 @@
+# jadczykjakub.pl
+
+## Description
+Page with a short description of my person and completed projects. 
+
+
+## Technologies
+Html 5
+Sass
+JavaScript
+
+
+## Author
+Jakub Jadczyk
--- js/main.js
+++ js/main.js
@@ -1,11 +1,6 @@
-//animation first page
-
 
 //smooth scroll
 const more = document.querySelector('.more a')
-
-
-
 
 const smoothScrool = (e) =>{
     e.preventDefault();
@@ -39,9 +34,9 @@ const pPort = document.querySelectorAll('.portfolio p');
 //variables contact
 const quote = document.querySelector('.contact .quote');
 const h1Cont = document.querySelector('.contact .h1Cont');
-const h2Cont = document.querySelector('.contact h2')
+const h2Cont = document.querySelector('.contact h2');
 
-const divLanguage = document.querySelector('.language')
+const divLanguage = document.querySelector('.languageChoice');
 divLanguage.addEventListener('click', ()=>{
     lgRadio.forEach(element => { if(element.checked){
         //remove last active
@@ -52,31 +47,21 @@ divLanguage.addEventListener('click', ()=>{
 
 
         let id = element.id;    
-
         //Top page
         h1Top.textContent = languages.Top[`${id}`].h1Top;
         pTop.textContent = languages.Top[`${id}`].pTop;
         aTop.textContent = languages.Top[`${id}`].aTop
-        
-
         //describe page
         h1Desc.textContent = languages.Desc[`${id}`].h1Desc;
-
         h2Desc.forEach((element, index) => { 
             element.textContent = languages.Desc[`${id}`].h2Desc[index];   
         });
-
-
         h3Desc.forEach((element, index) => {
             element.textContent = languages.Desc[`${id}`].h3Desc[index];
         });
-
         pDesc.forEach((element, index) => {
             element.textContent = languages.Desc[`${id}`].pDesc[index];
         });
-        
-
-
         // portfolio page
         h1Port.textContent = languages.Port[`${id}`].h1Port;
         
@@ -91,8 +76,6 @@ divLanguage.addEventListener('click', ()=>{
         quote.textContent = languages.Cont[`${id}`].quote;
         h1Cont.textContent = languages.Cont[`${id}`].h1Cont;
         h2Cont.textContent= languages.Cont[`${id}`].h2Cont;
-        // console.log(id)
-        //switch interests cube 
         if(id === "pol"){
             interests = interestsPl;
         }else if(id === "de"){
@@ -137,44 +120,44 @@ const languages = {
         },
     Desc: {
             pol: {
-                h1Desc: "Co potrafie",
+                h1Desc: "Co potrafie?",
                 h2Desc: ["Tworze strony internetowe", "Języki Obce"],
                 h3Desc: ["Nowoczene", "Responsywne", "Estetyczne", "Niemiecki B1", "Angielski C1"],
-                pDesc: ["Tworząc projekty zdobywam doświdaczenie rozszerzając swoją wiedze o nowe biblioteki/frameworki. Zwracam uwage na czytelność kodu, ulepszając go z każdym nowym projektem.", "Korzystając z media queries strony tworze tak by dostosowywały się do rożnych rozdzielczośći ekranów. Strony są czytelne dla wszystkich urządzeń które przeglądają strone", "Korzystając z programu PhotoShop edytuje/tworze grafiki tak by ich zawartość wpasowała sie w moje projekty. Dużą wage przykładam do spójności projektów od strony wizualnej", "Niemieckiego ucze się od 12 miesięcy, krótka ale intensywna nauka. Komunikacja na poziomie średnio-zaawansowanym  01.07.2020 program WorkAway, 4 miesiące mieszkania z niemiecką rodziną. ", "Angielskiego ucze sie od wielu lat. W tym języku na codzien oglądam filmy oraz czytam książki. Komunikacja nie sprawia mi żadnego problemu. 2x Work&Travel USA, 8 miesięcy pracy w USA"],
+                pDesc: ["Tworząc projekty, zdobywam doświadczenie rozszerzając swoją wiedzę o nowe biblioteki/frameworki. Zwracam uwage na czytelność kodu, ulepszając go z każdym nowym projektem.", "Korzystając z media queries, strony tworze tak by dostosowywały się do różnych rozdzielczości ekranów. Strony są czytelne dla wszystkich urządzeń które je przeglądają.", "Korzystając z programu Photoshop edytuje/tworze grafiki tak, by ich zawartość wpasowała się w moje projekty. Dużą wage przywiązuje do spójności projektów od strony wizualnej.", "Niemieckiego ucze się od 12 miesięcy, krótka ale intensywna nauka. Komunikacja na poziomie średnio-zaawansowanym.","3 miesiące mieszkania z niemiecką rodziną." , "Angielskiego ucze sie od wielu lat. W tym języku na codzien oglądam filmy oraz czytam książki.",  "2x 4 miesiące pracy w USA"],
                 
 
             },
             en: {
-                h1Desc: "What can I do",
+                h1Desc: "My skills",
                 h2Desc: ["I create websites", "foreign language"],
                 h3Desc: ["modern", "Responsive", "aesthetic", "German B1", "English C1"],
-                pDesc: ["By creating projects, I gain experience by expanding my knowledge with new libraries / frameworks. I pay attention to the readability of the code, improving it with each new project.", "By using media queries, pages are created to adapt to different screen resolutions. The pages are readable by all devices that view the page", "Using PhotoShop, I edit / create graphics so that their content fits into my projects. I attach great importance to the visual consistency of the projects", "I have been learning German for 12 months, short but intensive learning. Intermediate level communication 01/07/2020 WorkAway program, 4 months of living with a German family.", "I have been learning English for many years. I watch movies and read books on a daily basis in this language. Communication is not a problem for me.2x Work & Travel USA, 8 months of work in the USA"],
+                pDesc: ["By creating projects, I gain experience by expanding my knowledge with new libraries / frameworks. I pay attention to the readability of the code, improving it with each new project.", "By using media queries, pages are created to adapt to different screen resolutions. The pages are readable by all devices that view them.", "Using Photoshop, I edit / create graphics so that their content fits into my projects. He pays a lot of attention to the visual consistency of the projects.", "I have been learning German for 12 months, a short but intensive study. Intermediate level communication",   "3 months of living with a German family.", "I have been learning English for many years. I watch movies and read books on a daily basis in this language.",  "2x 4 months of work in the USA"],
 
             },
             de: {
-                h1Desc: "Was kann Ich machen",
+                h1Desc: "meine Fähigkeiten",
                 h2Desc: ["Ich erstelle Websites","fremdsprachen"],
                 h3Desc: ["modern", "Reactionschnell", "ästhetisch", "Deutsch B1", "Englisch C1"],
-                pDesc: ["Durch das Erstellen von Projekten sammle ich Erfahrung, indem ich mein Wissen mit neuen Bibliotheken / Frameworks erweitere. Ich achte auf die Lesbarkeit des Codes und verbessere ihn mit jedem neuen Projekt.", "Mithilfe von Media Queries werden Seiten erstellt, um sie an unterschiedliche Bildschirmauflösungen anzupassen. Die Seiten können von allen Geräten gelesen werden, die die Seite anzeigen", "Mit PhotoShop bearbeite / erstelle ich Grafiken so, dass ihr Inhalt in meine Projekte passt. Ich lege großen Wert auf die visuelle Konsistenz der Projekte", "Ich lerne seit 12 Monaten Deutsch, kurz aber intensiv. Kommunikation auf mittlerer Ebene 01/07/2020 WorkAway-Programm, 4 Monate bei einer deutschen Familie.", "Ich lerne seit vielen Jahren Englisch. Ich schaue täglich Filme und lese Bücher in dieser Sprache. Kommunikation ist für mich kein Problem. 2x Work & Travel USA, 8 Monate Arbeit in den USA"],
+                pDesc: ["Durch das Erstellen von Projekten sammle ich Erfahrung, indem ich mein Wissen mit neuen Bibliotheken / Frameworks erweitere. Ich achte auf die Lesbarkeit des Codes und verbessere ihn mit jedem neuen Projekt.", "Mithilfe von Medienabfragen werden Seiten erstellt, um sie an unterschiedliche Bildschirmauflösungen anzupassen. Die Seiten können von allen Geräten gelesen werden, die sie anzeigen.", "Mit Photoshop bearbeite / erstelle ich Grafiken so, dass ihr Inhalt in meine Projekte passt. Er achtet sehr auf die visuelle Konsistenz der Projekte.", "Ich lerne seit 12 Monaten Deutsch, ein kurzes, aber intensives Studium. Kommunikation auf mittlerer Ebene.","3 Monate bei einer deutschen Familie leben.", "Ich lerne seit vielen Jahren Englisch. Ich schaue täglich Filme und lese Bücher in dieser Sprache.",  "2x 4 Monate Arbeit in den USA"],
             }
         },
     Port: {
         pol: {
             h1Port: "Moje Projekty",
             h4Port: ['Konwerter Walut', 'Strona Fizjoterapia', 'Strona Handmade', 'In Progress'],
-            pPort: ['Konwerter pokazujący sprawdzić relacje dwóch dowolnych walut. Aktualne kursy walut z wykorzystaniem API.', 'Zagospodarowanie przestrzeni z użyciem Flexboxa. Nawigacja śledząca aktualną pozycje uzyskana za pomocą JavaScript.', 'Galeria uzyskana za pomocą biblioteki Lightbox. Form validation tylko frond-end oraz nawigacja śledząca pozycje za pomocą Javascript.', '...'],
+            pPort: ['Konwerter sprawdzający relacje dwóch dowolnych walut. Aktualne kursy walut z wykorzystaniem API.', 'Zagospodarowanie przestrzeni z użyciem Flexboxa. Nawigacja śledząca aktualną pozycje uzyskana za pomocą JavaScript.', 'aleria uzyskana za pomocą biblioteki Lightbox. Form validation frond-end oraz nawigacja śledząca pozycje za pomocą Javascript.', '...'],
 
         },
         en: {
             h1Port: "My Projects",
             h4Port: ['Currency Converter', 'Web Physiotherapy', 'Web Handmade', 'In Progess'],
-            pPort: ['Converter that shows you the relationship of any two currencies. Current exchange rates using API.', 'Development of space with the use of Flexbox. Navigation tracking the current position obtained using JavaScript.', ' Gallery obtained with the Lightbox library. Form validation only front-end and position tracking using Javascript. ', '...'],
+            pPort: ['Converter checking relations of any two currencies. Current exchange rates using API.', 'Development of space with the use of Flexbox. Navigation tracking the current position obtained using JavaScript.', ' Gallery obtained with the Lightbox library. Form validation front-end and position tracking using Javascript. ', '...'],
 
         },
         de: {
             h1Port: "Meine Projekte",
             h4Port: ['Converter', 'Web Physiotherapie', 'Web Handgemacht ', 'In progress'],
-            pPort: ['Konverter, der Ihnen die Beziehung zwischen zwei beliebigen Währungen zeigt. Aktuelle Wechselkurse über API.', 'Raumentwicklung mit Flexbox. Navigation zur Verfolgung der aktuellen Position, die mit JavaScript ermittelt wurde.', 'Galerie mit der Lightbox-Bibliothek erhalten. Formularvalidierung nur Front-End- und Positionsverfolgung mit Javascript.', '...'],
+            pPort: ['Konverter prüft die Beziehungen zweier beliebiger Währungen. Aktuelle Wechselkurse über API.', 'Raumentwicklung mit Flexbox. Navigation zur Verfolgung der aktuellen Position, die mit JavaScript ermittelt wurde.', 'Galerie mit der Lightbox-Bibliothek erhalten. Formularvalidierung Front-End und Positionsverfolgung mit Javascript.', '...'],
         }
     },
     Cont: {
@@ -196,10 +179,6 @@ const languages = {
         },
 
 }
-
-
-
-
 
 //cubic interests
 
@@ -223,7 +202,6 @@ const changeSide = ()=>{
         count = 0;
         interestText.textContent = interests[count]
     }
-    // console.log(count)
     cube.classList.remove(`${classCube[count-1]}`)
     count++
     
@@ -231,7 +209,6 @@ const changeSide = ()=>{
 }
 
 let intervalCube = setInterval(changeSide, 3000);
-// intervalCube()
 let flag = true;
 
 function clearOrResume(){
@@ -249,30 +226,3 @@ cube.addEventListener('click', clearOrResume)
 
 
 
-// hover
-
-// const divLang = document.querySelector('.language1')
-// const divShow = document.querySelector('.divShow')
-// let leftPx;
-// let topPx;
-
-// divLang.addEventListener('mousemove', hoverLang)
-
-
-// function hoverLang(e){
-//     leftPx = e.clientX
-//     topPx = e.clientY
-//     divShow.style.display = 'block'
-//     divShow.style.left = `${leftPx}px`
-//     divShow.style.top = `${topPx}px`
-//     console.log('siema')
-
-//     if(divShow)
-
-//     // console.log(divLang.getBoundingClientRect())
-
-
-// }
-
-
-// hoverLang()
